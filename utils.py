842d7d1b99ee34efad75d9c6c4b256b353f47f47
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-25 03:04:41 +0000

    Adding tarfile member sanitization to extractall()

--- utils.py
+++ utils.py
@@ -18,7 +18,26 @@ def download_voc(path, overwrite=False):
         filename = download(url, path=path, overwrite=overwrite, sha1_hash=checksum)
         # extract
         with tarfile.open(filename) as tar:
-            tar.extractall(path=path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=path)
 
 def make_train_val_pickle(path,train=1,val=1):
     if not (os.path.exists(os.path.join(path, 'VOC2007')) or os.path.exists(os.path.join(path, 'VOC2012'))):
